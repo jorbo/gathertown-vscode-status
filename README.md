0af43f5093bb11f1842ff89455d61200ebc2372e
Jordan Ayvazian <your@example.com>
2023-09-18 10:04:17 -0700

    Add gitlab issues as status and some readme instructions on how to get api keys.

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # gathertown-vscode-status README
 
 Simple extension that sets your Gather.town status to whatever you are working on in vscode
+
+
+## Setup
+
++ Get a gather.town api key from (https://app.gather.town/apikeys)[https://app.gather.town/apikeys]
++ The spaceid is everything after https://app.gather.town/app/
+    -  I.E. (CVasGasjasdsdsas/yourspace) but change the forward slash to a backslash
+
+### For gitlab 
++ Make a personal access token with api and read_api scope (https://gitlab.com/-/profile/personal_access_tokens)[https://gitlab.com/-/profile/personal_access_tokens]
++ Turn off show status and show manual status (Will eventually change this)
--- src/extension.ts
+++ src/extension.ts
@@ -11,23 +11,39 @@ var game: Game;
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	let output = vscode.window.createOutputChannel("GatherTown");
-	output.show();
-	var apikey = vscode.workspace.getConfiguration("gathertown").get("apikey") as string;
+
+
+	var apiKey = vscode.workspace.getConfiguration("gathertown").get("apikey") as string;
 	var spaceid = vscode.workspace.getConfiguration("gathertown").get("spaceid") as string;
-	output.appendLine("API Key: " + JSON.stringify(apikey));
-	output.appendLine("spaceid " + JSON.stringify(spaceid));
+	var url = vscode.workspace.getConfiguration("gathertown").get("gitlabUrl") as string;
+	var gitlabToken = vscode.workspace.getConfiguration("gathertown").get("gitlabToken") as string;
+	output.appendLine("API Key: " + apiKey);
+	output.appendLine("spaceid " + spaceid);
+
+
 	// gather game client setup
-	game = new Game(spaceid, () => Promise.resolve({ apiKey: apikey }));
+	game = new Game(spaceid, () => Promise.resolve({ apiKey: apiKey }));
 	game.connect();
 	game.subscribeToConnection((connected) => {		
 		output.appendLine("connected? " + connected);
 	});
+	var issueId = context.globalState.get("issueId");
+	var issueProjectId = context.globalState.get("issueProjectId");
+	output.appendLine(`issueId: ${issueId}`);
+	output.appendLine(`issueProjectId: ${issueProjectId}`);
+
+	if(issueId && issueProjectId){		
+		axios.get(`${url}/api/v4/projects/${issueProjectId}/issues/${issueId}?private_token=${gitlabToken}`, {headers:{"PRIVATE-TOKEN":gitlabToken}}).then((response) => {
+			game.setTextStatus(`#${issueId} ${response.data.title}`);
+		}).catch((error) => {
+			output.appendLine(error);
+		});
+	}
+	
 	vscode.window.onDidChangeActiveTextEditor((editor: any) => {
 		var workspaceName = vscode.workspace.name;
 		var filePath = vscode.window.activeTextEditor?.document.fileName;
 		var fileName = filePath?.slice(filePath?.lastIndexOf('/')+1, filePath.length);
-		output.appendLine("workspace: " + workspaceName);
-		output.appendLine("file: " + fileName);
 		var status = "";
 		if(vscode.workspace.getConfiguration("gathertown").get("showWorkspaceName") as boolean){
 			status += workspaceName + " ";
@@ -60,38 +76,56 @@ export function activate(context: vscode.ExtensionContext) {
 
 	let disposable2 = vscode.commands.registerCommand('gathertown.setStatusFromGitlabIssue', async () => {
 		var id = "";
-		var apiKey = vscode.workspace.getConfiguration("gathertown").get("gitlabToken") as string;
-		var url = vscode.workspace.getConfiguration("gathertown").get("gitlabUrl") as string;
-		if(vscode.workspace.getConfiguration("gathertown").get("groupOrProject") === "group"){
-			id = vscode.workspace.getConfiguration("gathertown").get("gitlabGroupId") as string;
-		} else {
+		var groupsOrProjects = vscode.workspace.getConfiguration("gathertown").get("groupOrProject");
+		if(groupsOrProjects === "groups"){
+			id = vscode.workspace.getConfiguration("gathertown").get("gitlabGroupId") as string;			
+		}
+		else {
 			id = vscode.workspace.getConfiguration("gathertown").get("gitlabProjectId") as string;
 		}
-		axios.get(url + "/api/v4/groups/" + id + "/issues?state=opened&private_token=" + apiKey, {headers:{"PRIVATE-TOKEN":apiKey}}).then((response) => {
+		axios.get(`${url}/api/v4/${groupsOrProjects}/${id}/issues?private_token=${gitlabToken}&state=opened`, {headers:{"PRIVATE-TOKEN":gitlabToken}}).then((response) => {
 			var issues = response.data;
 			var options = issues.map((issue: any) => {
 				return {
+					iid: issue.iid,
+					projectId: issue.project_id,
+					lables: issue.labels,
 					label: `#${issue.iid} ${issue.title}`,
 					description: issue.web_url
 				};
 			});
 			vscode.window.showQuickPick(options).then((issue:any) => {
-				let output = vscode.window.createOutputChannel("GatherTown");
-				output.show();
-				if(issue){				
+				if(issue){			
 					game.setTextStatus(issue.label);
+					output.appendLine(`Labels: ${JSON.stringify(issue)}}`);
+					output.appendLine(`Labels: ${issue.lables as string[]}`);
+					var issueIid = issue.iid;
+					var issueProjectId = issue.projectId;
+					context.globalState.update("issueId", issueIid);;
+					context.globalState.update("issueProjectId", issueProjectId);
+					axios.put(`${url}/api/v4/projects/${issue.projectId}/issues/${issue.iid}?private_token=${gitlabToken}&remove_labels=Essential List&add_labels=Doing - Active`, {headers:{"PRIVATE-TOKEN":gitlabToken}}).then((response) => {
+						output.appendLine(JSON.stringify(response.headers));
+					}).catch((error) => {
+						output.appendLine(error);
+					});
 				}
 			});
+		}).catch((error) => {
+			output.appendLine(error);
 		});
+
+
 	});
 	context.subscriptions.push(disposable2);
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "gathertown-vscode-status" is now active!' + apikey);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {
 	game.setTextStatus(``);	
+	game.disconnect().then(() => {
+		console.log("disconnected");
+	});
 }
 
